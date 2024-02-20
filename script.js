42f62e24595867a2370b175623093202cd727af0
EnteraUsernametostart <127363537+EnteraUsernametostart@users.noreply.github.com>
2024-02-19 22:38:41 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function Haha() {
+  // Select the button element by its ID
+  var button = document.getElementById('myButton');
+
+  // Change the button text
+  button.innerText = 'ha dumbass';
+}
